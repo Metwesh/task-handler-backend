e96b32783de68c6d08e28023613349c67e584ffd
Mohamed H. Aly <94052626+Metwesh@users.noreply.github.com>
2022-04-23 04:03:10 +0200

    Fixed stuff

--- src/server.js
+++ src/server.js
@@ -4,7 +4,6 @@ const cors = require("cors");
 
 require("dotenv").config({ path: "./config.env" });
 
-const port = process.env.PORT || 3001;
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
